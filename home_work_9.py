39950f7acb50f2f3c0e1d2a4a24d6ae1a1f28a08
Kirill863 <Byblik4310@gmail.com>
2025-02-04 20:43:17 +0300

    добавли фильтрацию списка фильмов c использованием генератора словарей

--- home_work_9.py
+++ home_work_9.py
@@ -1,10 +1,21 @@
 from marvel import full_dict
 
 def user_value(value):
+#  функция для превращения, вводимой пользователем строки в список
+# принимает - стороку с цифрами, возвращает - список с цифрами
     try:
         return int(value)
     except ValueError:
         return None
 user_input = input("Вводите числа через пробел")
 numbers = list(map(user_value, user_input.split()))
-print(numbers)
+
+
+valid_ids = list(filter(lambda x: x is not None, numbers))
+# Удаление None из списка valid_ids
+
+filtered_movies_dict = {movie_id: full_dict[movie_id] for movie_id in valid_ids if movie_id in full_dict}
+
+# Фильтрация списка фильмов на основе valid_ids c использованием генератора словарей
+
+print(filtered_movies_dict)
